496b5098f0ac0e5c2edf5cef40d11d67d836ad1f
Dana Hatem <s-dana.abouzeid@zewailcity.edu.eg>
2025-12-07 02:51:39 +0200

    Add booking page; updated services page

---
+++ src/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(
+    __name__,
+    template_folder="../templates",  # templates folder
+    static_folder="../templates"     # CSS, images, fonts folder
+)
+
+from routes import *
+
+if __name__ == "__main__":
+    app.run(debug=True)
